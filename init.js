51983efbf345956eea2dd800d4b5939f96247792
andreinavega <andrevesu16@gmail.com>
2023-06-27 12:03:19 -0300

    Init

---
+++ init.js
@@ -0,0 +1,32 @@
+// Operadores lógicos
+// ------------------
+// ===
+// !==
+// &&
+// ||
+
+// Buleanos
+// --------
+// Boolean
+// truthy
+// falsy
+
+// Algunos métodos que devuelven true/false
+// ----------------------------------------
+// .includes (Array|String.prototype.includes)
+// .contains (HTMLElem, classList)
+// .every .some (Array.prototype)
+
+// Función que muestra mensaje en consola
+// si el nombre del usuario es Andreina
+// y otro mensaje adicional si la nota es mayor que 7
+function logAndreinaQ (nombre, nota) {
+    
+}
+
+const nombre = prompt('Ingrese su nombre:');
+const nota = prompt('Ingrese su nota:');
+logAndreinaQ(nombre, nota)
+
+// Andreina
+
